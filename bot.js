0bb314cf67bb1483956bf19f5500c980ea630ab1
xMoha <40671043+xMoha@users.noreply.github.com>
2018-07-11 11:07:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '.'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Safri Gmaes`,"http://twitch.tv/S-F")
+client.user.setGame(`Safri Games`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
