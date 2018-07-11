e70403daa1b94154b4d00ccaf41e469c04f41410
xMoha <40671043+xMoha@users.noreply.github.com>
2018-07-11 11:06:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,3 +30,28 @@ client.user.setGame(`Safri Gmaes`,"http://twitch.tv/S-F")
 
 client.login(process.env.BOT_TOKEN);
 
+
+if (message.content == "اعلام") {
+        var x = [ 'جيب علم امريكا ','جيب علم تركيا' , 'جيب علم السعوديه ' , 'جيبلي علم الامارات'];
+        var x2 = [ '🇺🇸','🇹🇷','🇸🇦','🇦🇪'];
+        var x3 = Math.floor(Math.random()*x.length)
+        message.channel.send(` :${x[x3]}, لديك 15 ثانية`).then(msg1=> {
+            var r = message.channel.awaitMessages(msg => msg.content == x2[x3], {
+                maxMatches : 1, 
+                time : 15000, 
+                errors : ['time'] 
+            })
+        r.catch(() => {
+            return message.channel.send('❌ لقد انتهى الوقت ولم يقم أحد بالأجابة بشكل صحيح') 
+               message.channel.sendEmbed(embed)
+        })
+        r.then(s=> {
+
+            points[message.author.id].points +=1
+            message.channel.send(`✅ لقد قمت بكتابة الجواب الصحيح بالوقت المناسب
+ ─═════**{نقاطك:${points[message.author.id].points}}**═════─`);
+               message.channel.sendEmbed(embed)
+        })
+        })
+    }
+
